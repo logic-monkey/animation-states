d7d9838c96c9cc679a100f20d5cb0eb66770940f
Logic Monkey <logic.monkey@protonmail.com>
2024-01-08 11:43:42 -0600

    Added signal to passthru animation states.

--- passthru.gd
+++ passthru.gd
@@ -4,9 +4,12 @@ class_name ST_PassThru
 @export var animation := ""
 @onready var anim = %AnimationPlayer
 
+signal transition_done
 func enter(previous_state = "", _msg: Dictionary = {}):
 	if animation.is_empty(): return
 	anim.play(animation)
 	await anim.animation_finished
 	if not active or not "to" in _msg: return
 	transition(_msg.to, _msg)
+	transition_done.emit()
+	
