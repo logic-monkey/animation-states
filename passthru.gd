0a58ae7b5739767822ee08fa61e30f7309fe1d8b
Logic Monkey <logic.monkey@protonmail.com>
2024-01-10 05:51:32 -0600

    Tweaked in accordance with the needs of sound and slash animations.

--- passthru.gd
+++ passthru.gd
@@ -3,13 +3,17 @@ class_name ST_PassThru
 
 @export var animation := ""
 @onready var anim = %AnimationPlayer
+@export var sound_effect : AudioStreamPlayer2D
 
 signal transition_done
 func enter(previous_state = "", _msg: Dictionary = {}):
 	if animation.is_empty(): return
+	if sound_effect: sound_effect.play()
 	anim.play(animation)
 	await anim.animation_finished
-	if not active or not "to" in _msg: return
+	if not active: 
+		transition_done.emit()
+		return
 	transition(_msg.to, _msg)
 	transition_done.emit()
 	
